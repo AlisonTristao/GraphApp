bee00f5d511b0938f8bc252047a4709401bcb586
AlisonTristao <AlisonTristao@hotmail.com>
2022-08-18 17:50:59 -0300

    Versão final 1.0

--- graph/app/src/main/java/com/example/graph/MainActivity.java
+++ graph/app/src/main/java/com/example/graph/MainActivity.java
@@ -1,18 +1,27 @@
 package com.example.graph;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.androidplot.util.PixelUtils;
@@ -27,14 +36,21 @@ import com.androidplot.xy.XYGraphWidget;
 import com.androidplot.xy.XYPlot;
 import com.androidplot.xy.XYSeries;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.FieldPosition;
 import java.text.Format;
+import java.text.ParseException;
 import java.text.ParsePosition;
 import java.util.Arrays;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
+    // botoes e cards
     public Boolean rola = true;     // salva se o grafico esta rolando ou nao
+    public Button btnConectar;     // conexap bt
     public Button btnCopiar;        // copia o array pra area de trasferecencia
     public Button btnEnviar;        // envia as constantes pro carrinho
     @SuppressLint("UseSwitchCompatOrMaterialCode")
@@ -42,13 +58,36 @@ public class MainActivity extends AppCompatActivity {
     public Button btnConf;          // abre as conf
     public Button btnX;             // fecha o cardConfig
     public Button btnLimpar;        // limpa o grafico
-    public XYPlot grafico;          // grafico
-    public XYSeries linha;          // linha
-    public Number[] valores = {0};  // valores do grafico
     public CardView cardConf;       // card das configurações
     public EditText edtKe;          // txt q tem os valores do Ke
     public EditText edtKd;          // txt q tem os valores do Kd
     public EditText edtKi;          // txt q tem os valores do Ki
+    public EditText edtVel;         // txt q tem o valor da velocidade
+
+    // variaveis usadas no grafico
+    public XYPlot grafico;          // grafico
+    public XYSeries linha;          // linha
+    public Number[] valores = {0};  // valores do grafico
+
+    //variaveis usadas no bluetooth
+    BluetoothAdapter adapBT = null;         // adaptador bluetooth
+    BluetoothDevice devBT = null;           // dispositipo bluetooth
+    BluetoothSocket socBT = null;           // entrada/canal bluetooth
+    public boolean con = false;             // conectado ou nao
+    static String endMac = null;            // endereço mac do dispositivo
+    Handler manipulador = null;             // manipulador dos dados recebidos por bt
+    // salva os dados recebidos por bt
+    StringBuilder dadosRecebidosBT = new StringBuilder();
+
+    /*maracutaia pra comunicação bt (todas os dispositivos devem ter esse UUID)
+    cada dispositivo tem uma porta UUID diferente, então vai dar erro se tentar conectar com algo
+    diferente (computador, tablet...)*/
+    UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+
+    // (cada request tem um int para sua identificação)
+    public static final int REQ_BT = 1;     // requisição da ativação bt
+    public static final int REQ_CON = 2;    // requisição para conexão bt
+    private static MainActivity.conTh conTh;
 
     @SuppressLint("WrongViewCast")
     public void iniciaComponentes(){
@@ -64,6 +103,8 @@ public class MainActivity extends AppCompatActivity {
         edtKe = findViewById(R.id.txtKe);
         edtKd = findViewById(R.id.txtKd);
         edtKi = findViewById(R.id.txtKi);
+        btnConectar = findViewById(R.id.btnBlue);
+        edtVel = findViewById(R.id.txtVelocidade);
 
         // define como nao no swt de rolar o grafico
         swtRolar.isChecked();
@@ -72,6 +113,7 @@ public class MainActivity extends AppCompatActivity {
         cardConf.setVisibility(View.INVISIBLE);
     }
 
+    @SuppressLint({"MissingPermission", "HandlerLeak"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // tela q vai abrir
@@ -85,13 +127,54 @@ public class MainActivity extends AppCompatActivity {
         //inicia os componentes
         iniciaComponentes();
 
+        // -------- inicia o grafico -------- //
+
         // cria o grafico
         criaGrafico();
-
         // plota o grafico
         plotaGraf(grafico);
 
+        // -------- inicia o bluetooth -------- //
+
+        adapBT = BluetoothAdapter.getDefaultAdapter();
+        // verifica se o dispositivo possui bluetooth
+        if (adapBT == null) {
+            // dispositivo não suporta bluetooth
+            Toast.makeText(this, "Seu dispositivo não possui Bluetooth!",
+                    Toast.LENGTH_SHORT).show();
+            // é preciso verifica pois se tentar ligar o bt sem ter bt crasha o app
+        } else if (!adapBT.isEnabled()) { // verifica se o bluetooth está desligado
+            // pede pra ligar o bluetooth
+            // (aqui da um erro q n sei o pq, eu suprimi e funcionou ok)
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQ_BT);
+        }
+
         //-----------------------------// botes //----------------------------//
+
+        // abre a lista de dispositivos pareados ou desconecta
+        btnConectar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // se não estiver conectado abre a lista de dispositivos
+                if (con) {// se estiver conectado
+                    try{// tenta desconectar
+                        socBT.close();
+                        Toast.makeText(MainActivity.this, "Desconectado!",
+                                Toast.LENGTH_SHORT).show();
+                        // define como desconectado
+                        con = false;
+                        btnConectar.setText("Bluetooth");
+                    } catch (IOException er) {//mensagem de erro
+                        Toast.makeText(MainActivity.this, "Erro ao desconectar!",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                } else {// senão abre uma lista com dispositivos pareados
+                    Intent abreList = new Intent(MainActivity.this, Dispositivos.class);
+                    startActivityForResult(abreList, REQ_CON);
+                }
+            }
+        });
 
         // bluetooth
         btnConf.setOnClickListener(new View.OnClickListener() {
@@ -123,10 +206,16 @@ public class MainActivity extends AppCompatActivity {
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // {ke,kd,ki}
-                String texto = "{"+edtKe.getText()+","+edtKd.getText()+","+edtKi.getText()+"}";
+                // {ke,kd,ki,velocidade}
+                String texto = "{"+edtKe.getText()+","+
+                                edtKd.getText()+","+edtKi.getText()+","+edtVel.getText()+"}";
 
                 // enviar por bluetooth
+                if(con){
+                    MainActivity.conTh.enviar(texto);
+                }else{
+                    Toast.makeText(MainActivity.this, "Desconectado!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -155,14 +244,47 @@ public class MainActivity extends AppCompatActivity {
         // limpa os dados do grafico
         btnLimpar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {valores = new Number[]{};}});
+            public void onClick(View view) {
+                valores = new Number[]{};
+                appendLinha(0);
+            }
+        });
 
         btnX.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {cardConf.setVisibility(View.INVISIBLE);}});
 
-        // isso aqui vou apagar
-        new th().start();
+        //-----------------------------// recebe dados //----------------------------//
+
+        // fica monitorando sozinho quando recebe mensagem
+        manipulador = new Handler(){
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+
+                // se a mensagem ser "message_read" = 0
+                if(msg.what == 0){
+                    // transforma em string
+                    String recebidos = (String) msg.obj;
+
+                    // junta os dados enquanto esta recebendo algo
+                    dadosRecebidosBT.append(recebidos);
+
+                    // o codigo de antes estava desatualizado e n funcionava direito
+                    // assim ele ja funciona ok
+
+                    //converte obg pra string e depois interio e add na linha
+                    try {
+                        appendLinha(Integer.parseInt(String.valueOf(dadosRecebidosBT)));
+                    } catch (NumberFormatException e) {
+                        Toast.makeText(MainActivity.this, "Erro ao receber dados!",
+                                                                    Toast.LENGTH_SHORT).show();
+                    }
+
+                    // limpa a variavel
+                    dadosRecebidosBT.delete(0, dadosRecebidosBT.length());
+                }
+            }
+        };
     }
 
     protected String copiaDados(){
@@ -178,8 +300,11 @@ public class MainActivity extends AppCompatActivity {
         // salva as constantes
         String constatnes = "Ke: "+edtKe.getText()+" Kd: "+edtKd.getText()+" Ki: "+edtKi.getText();
 
+        // salva a velocidade
+        String vel = "Velocidade: " + edtVel.getText();
+
         // retorna as constantes mais o grafico
-        return(constatnes + "\n" + dadosGrafico);
+        return(constatnes + "\n" + vel + "\n" + dadosGrafico);
     }
 
     protected void criaGrafico(){
@@ -219,8 +344,6 @@ public class MainActivity extends AppCompatActivity {
         // adiciona a linha no grafico novamente
         grafico.addSeries(linha, formatoLinha);
 
-
-
         // redesenha as linhas
         grafico.redraw();
 
@@ -252,26 +375,122 @@ public class MainActivity extends AppCompatActivity {
         return arr;
     }
 
-    public class th extends Thread{// função q vai executar em segundo plano
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {// verifica qual a requisição feita
+            case REQ_BT:// caso for uma requisição para ligar o bt
+                if (resultCode == Activity.RESULT_OK) {// se clicou ok
+                } else {// se não deixou ligar o bt
+                    Toast.makeText(this, "Erro ao ligar o Bluetooth!",
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            case REQ_CON:// caso seja requisição para conectar em um dispositivo
+                if (resultCode == Activity.RESULT_OK) {// temos o endereço mac que retorna da lista
+                    // salva o endereço mac q retornou
+                    endMac = data.getExtras().getString(Dispositivos.endMac);
+
+                    // buscamos o dispositivo bluetooth pelo seu endereço mac
+                    devBT = adapBT.getRemoteDevice(endMac);
+
+                    // criamos um canal de comunicação e começamos a enivar os dados
+                    try {
+                        // criamos um canal de comunicação com UUID
+                        socBT = devBT.createInsecureRfcommSocketToServiceRecord(uuid);
+
+                        // conecta com o dispostivo
+                        socBT.connect();
+
+                        // define os fluxos de entrada e saida de dados
+                        conTh = new conTh(socBT);
+                        conTh.start();
+
+                        // mensagem
+                        Toast.makeText(this, "Conectado com: " + endMac,
+                                Toast.LENGTH_SHORT).show();
+
+                        // define como conectado
+                        con = true;
+                        btnConectar.setText("Desconectar");
+                    }catch (IOException er){// mensagem de erro
+                        Toast.makeText(this, "Erro ao criar comunicação bluetooth!",
+                                Toast.LENGTH_SHORT).show();
+
+                        // define como desconectado
+                        con = false;
+                        btnConectar.setText("Bluetooth");
+                    }
+                }else{// nao conseguiu obeter o endereço mac
+                    Toast.makeText(this, "Erro ao obeter endereço Mac!",
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    // classe para enviar e receber dados pela porta/canal socket
+    private class conTh extends Thread {
+        private final InputStream mmInStream;// saida
+        private final OutputStream mmOutStream;// entrada
+
+        public conTh(BluetoothSocket socket) {
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Obtem o fluxo de dados de entrada e saida
+            try {
+                tmpIn = socket.getInputStream();
+            } catch (IOException e) {
+                Toast.makeText(MainActivity.this, "Erro no InputStream!",
+                        Toast.LENGTH_SHORT).show();
+            }
+            try {
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+                Toast.makeText(MainActivity.this, "Erro no OutputStream!",
+                        Toast.LENGTH_SHORT).show();
+            }
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        // recebe os dados do bt
+        // modificamos pq o metodo do google n detecta se a mensaagem veio quebrada
         public void run() {
+            byte[] mmBuffer = new byte[1024];
+            int numBytes; // bytes returned from read()
 
-            int[] valores = {1, -1, 3, 2, 5, 7, 3, -2, -7, 4, 5, 7, 6, 0, -1, -2, -3};
-            int a = 0;
-
-            while (a < valores.length) {
+            while (true) {
                 try {
-                    sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    // Read from the InputStream.
+                    numBytes = mmInStream.read(mmBuffer);
+
+                    // string do buffer recebido em butes
+                    String dadosBT = new String(mmBuffer, 0, numBytes);
+
+                    // isso aqui detecta a mensagem lida
+                    manipulador.obtainMessage(0, numBytes, -1, dadosBT).sendToTarget();
+                } catch (IOException e) {
+                    // acho q crash se der erro
+                    break;
                 }
+            }
+        }
 
-                appendLinha(valores[a]);
-                a++;
 
-                if(a == valores.length){
-                    a = 0;
-                }
-
+        // enviar dados
+        public void enviar(String texto) {
+            // converte String pra um vetor de bytes
+            byte[] bytes = texto.getBytes();
+            try{
+                // fluxo de saida (envia os bytes)
+                mmOutStream.write(bytes);
+            } catch (IOException e) {
+                Toast.makeText(MainActivity.this, "Erro ao enviar dados!",
+                        Toast.LENGTH_SHORT).show();
             }
         }
     }
